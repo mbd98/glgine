248a1615c583aec61921b5e8db650c11e621e880
Mark Boudreau <mboudreaudalton@gmail.com>
2022-08-05 04:20:28 -0400

    asset cleanup

--- src/tracks.cpp
+++ src/tracks.cpp
@@ -7,7 +7,13 @@ Tracks::Tracks()
 	segments[2] = new Reusable(new Model("track_slight"));
 	segments[3] = new Reusable(new Model("track_180"));
 
-	segments[0]->insertTransform(glm::mat4(1.0f));
+	for (auto &s : segments)
+		s->setParent(this);
+
+	//segments[0]->insertTransform(glm::mat4(1.0f));
+	//segments[0]->insertTransform(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 150.0f)));
+	segments[3]->insertTransform(glm::mat4(1.0f));
+	segments[3]->insertTransform(glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
 }
 
 Tracks::~Tracks()
